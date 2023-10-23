c0a37b7049421e888d16ecf867f4cb82710a6e3c
viveksojitra06 <vs.designs94@gmail.com>
2023-10-23 15:10:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Axiform from './Axiform';
+import Classcom from './Components/Classcom';
+import Event from './Components/Event';
+import Explorer from './Components/Explorer';
+import A from "./Components/Hoc/A"
+import Myform from './Components/Myform';
+import Register from './Components/Register';
+import Darkmode from './Darkmode'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      {/* <h1>vicky</h1> */}
+      {/* <Explorer /> */}
+      {/* <Myform /> */}
+      {/* <A /> */}
+      {/* <Event /> */}
+      {/* <Register /> */}
+      <Axiform />
+      {/* <Darkmode/> */}
+    </div>
+  );
+}
+
+export default App;
